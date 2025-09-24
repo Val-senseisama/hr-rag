32d4a3089adddcd60c2a3f1bb08a3f7b96d51a67
Val <valizevbigiedavid@gmail.com>
2025-09-24 15:56:49 +0100

    Update CORS configuration to allow requests from the production URL

--- server/dist/index.js
+++ server/dist/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://hr-rag.vercel.app/"],
     credentials: true,
 }));
 app.use("/api", usersRouter);
